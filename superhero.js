a2b75699e2dc6aa63d5b209ffc3aeb2602623a21
JCahill <cahilljm@aol.com>
2022-03-19 20:12:48 -0700

    Push changes to event listener and for loop

--- superhero.js
+++ superhero.js
@@ -1,51 +1,83 @@
-//https://gateway.marvel.com:443/v1/public/characters?name=Spider%20Man&orderBy=name&limit=50&apikey=192c597a69f5e5f60181fcd4569e8f79
-//https://gateway.marvel.com:443/v1/public/characters?name=Thor&orderBy=name&limit=20&api-key=192c597a69f5e5f60181fcd4569e8f79
-const referrer = `curl --referer cahill53.github.io `
 const BASE_URL = `https://gateway.marvel.com:443/`;
 const resourceType = `v1/public/characters`
 const API_KEY = '192c597a69f5e5f60181fcd4569e8f79';
-const heroName = 'Thor';
-const nameStartsWith = 'Sp'
-const orderBy = 'name';
-const limit = 50;
 const hash = `07dced18d8feafe4974a0029ce3e1003`
 
-//const url = `${BASE_URL}${resourceType}?name=${heroName}&orderBy=${orderBy}&limit=${limit}&ts=1&apikey=${API_KEY}&hash=${hash}`;
-const url = `${BASE_URL}${resourceType}?nameStartsWith=${nameStartsWith}&orderBy=${orderBy}&limit=${limit}&ts=1&apikey=${API_KEY}&hash=${hash}`;
+const containerDiv = document.getElementById("container");
+const formEl = document.getElementById("hero-form");
+const heroInput1El = document.getElementById("heroNameStart");
 
-const containerDiv = document.getElementById('container');
-const buttonEl = document.getElementsByTagName('button')[0];
+//Upon submit fetch data from the API and populate the page
 
-console.log(url);
-console.log(buttonEl);
+formEl.addEventListener('submit', function (e) {
+  e.preventDefault();
 
- //Upon submit fetch data from the API and populate the page
- 
-// buttonEl.addEventListener('submit', function (e) {
-//   e.preventDefault();
+  const nameStartsWith = heroInput1El.value;
+  const orderBy = 'name';
+  const limit = 50;
+  const url = `${BASE_URL}${resourceType}?nameStartsWith=${nameStartsWith}&orderBy=${orderBy}&limit=${limit}&ts=1&apikey=${API_KEY}&hash=${hash}`;
 
-   // Fetch heros
+  // console.log(url);
+  // console.log(containerDiv);
+  // console.log(formEl);
+  // console.log(heroInput1El);
+  // console.log(nameStartsWith);``
+
+  // Fetch heros
   fetch(url)
     .then(function (data) {
       return data.json();
     })
+    //end of fetch
 
-//DEFINE THE ARRAY OF HEROS THAT RETURNS FROM THE FETCH
+    //DEFINE THE ARRAY OF HEROS THAT RETURNS FROM THE FETCH
     .then(function (responseJson) {
-      console.log(responseJson);
+      //return js object from json
+      console.log(responseJson.data);
 
-//DEFINE HEROS THAT WILL DISPLAY ON THE PAGE
+      //display array of objects returned
+      const heroResults = responseJson.data.results;
+      console.log(heroResults);
+
       for (let i = 0; i < array.length; i++) {
- 
-        const heros = responseJson.results[i];
-        console.log(heros);
 
-//DEFINE VARIABLES THAT WILL BE NEEDED TO POPULATE THE PAGE
+          const hero = responseJson.data.results[i];
+          console.log(hero);
+
+          const heroId = hero.id;
+          const heroName = hero.name;
+          const heroDescr = hero.description;
+          const thumbPath = hero.thumbnail.path
+          const thumbExt = hero.thumbnail.extension
+          const heroThumbnail = `${thumbPath}.${thumbExt}`
+
+          console.log(heroId);
+          console.log(heroName);
+          console.log(heroDescr);
+          console.log(heroThumbnail);
+
       }
+      //end of for loop
+
+      // for (const i in heroResults) {
+      //   if (heroResults.hasOwenProperty(i)) {
+      //     let hero = heroResults[i]
+
+      //     console.log(hero);
+      //     const name = hero.name;
+      //     console.log(name);
+      //   }
+      // }
+      //end of for if loop
+
     })
-//CONSOLE A MESSAGE IF THE FETCH IS NOT SUCCESSFUL
+    //end of then
+
+    //CONSOLE A MESSAGE IF THE FETCH IS NOT SUCCESSFUL
     .catch(function () {
       console.log(`No data returned from the fetch request`)
     })
+  //end of catch
 
-// });
+});
+//end of event listener
